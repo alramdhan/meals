065fb55dd626b523f4dec4fee43dfc3a08630ad8
Dika <al.ramdhan312@gmail.com>
2025-07-03 17:09:09 +0700

    Section 10 - 202 Done

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,18 +5,44 @@ import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
-    required this.onToggleFavorite,
     required this.availableMeals
   });
 
-  final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      // lowerBound: 0,
+      // upperBound: 1
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    final filteredMeals = widget.availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
 
     Navigator.push(
       context, 
@@ -24,7 +50,6 @@ class CategoriesScreen extends StatelessWidget {
         builder: (_) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
-          onToggleFavorite: onToggleFavorite,
         )
       )
     );
@@ -32,19 +57,34 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+        children: availableCategories.map((category) => CategoryGridItem(
+          category: category,
+          onSelectCategory: () {
+            _selectCategory(context, category);
+          }
+        )).toList()
       ),
-      children: availableCategories.map((category) => CategoryGridItem(
-        category: category,
-        onSelectCategory: () {
-          _selectCategory(context, category);
-        }
-      )).toList()
+      builder: (ctx, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, .3),
+          end: const Offset(0, 0)
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController, curve: Curves.easeInOut
+          )
+        ),
+        child: child
+      )
     );
   }
 }
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/providers/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
+class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
-    required this.onToggleFavorite
   });
 
   final Meal meal;
-  final void Function(Meal meal) onToggleFavorite;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -24,9 +27,19 @@ class MealDetailsScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              onToggleFavorite(meal);
+              final wasAdded = ref
+                .read(favoriteMealsProvider.notifier)
+                .toggleMealFavoriteStatus(meal);
+
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  duration: const Duration(seconds: 4),
+                  content: Text(wasAdded ? 'Meal added as a favorite.' : 'Meal removed.')
+                )
+              );
             },
-            icon: const Icon(Icons.star_border)
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border)
           )
         ],
       ),
